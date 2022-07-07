9c50c09f84df7742f103ff541704a12bdff73380
GabrielV <vitullo-veku@hotmail.com>
2022-07-07 00:12:29 -0300

    PrimerrepoJavascrip

---
+++ java/1er_desafio_complementario.js
@@ -0,0 +1,25 @@
+
+ let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+ for (let i = 1; i <= 3; i++) {
+     let resultado = ingresarNumero + i ;
+     alert(ingresarNumero + " + "+ i +" = "+ resultado);
+      continue
+    }
+
+let entrada = prompt("Ingresar un nombre de un banda"); {
+       switch (entrada) {
+           case "Metallica":
+                alert("Acertaste");
+                break;
+            case "Guns and Roses":
+                alert("Correcto");
+                break;
+           default:
+               alert("No es")
+               break;
+         }
+       while(entrada != "ESC" ) {
+
+       }
+      
+    }
